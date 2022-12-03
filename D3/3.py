1b705f24e6af465b088476ca3e6b075d2ee33bf6
Alexandre Duverger <alexandre.duverger@protonmail.com>
2022-12-03 10:09:38 -0500

    refacto

--- D3/3.py
+++ D3/3.py
@@ -5,34 +5,28 @@ def get_priority(item: str) -> int:
     return ordinal - 38
 
 
-groups = []
-group_count = 3
 p1_sum_priorities = 0
 p2_sum_priorities = 0
 
+# PART 1
 for line in open("3.txt"):
     line = line.strip()
-    # PART 1
     mid = len(line) // 2
-    first_compartment = line[:mid]
-    second_compartment = line[mid:]
-    for item in second_compartment:
-        if item in first_compartment:
+    comp_1, comp_2 = line[:mid], line[mid:]
+    for item in comp_2:
+        if item in comp_1:
             p1_sum_priorities += get_priority(item)
             break
-    # PART 2
-    if group_count >= 3:
-        groups.append([line])
-        group_count = 0
-    else:
-        groups[-1].append(line)
-    group_count += 1
 
-for group in groups:
-    for item in group[0]:
-        if item in group[1] and item in group[2]:
+# PART 2
+lines = [line for line in open("3.txt")]
+i = 0
+while i < len(lines):
+    for item in lines[i]:
+        if item in lines[i + 1] and item in lines[i + 2]:
             p2_sum_priorities += get_priority(item)
             break
+    i += 3
 
 print(f"P1: {p1_sum_priorities}")
 print(f"P2: {p2_sum_priorities}")
